08ee17ca5abe568e659173cf695ce604a2908592
Seng Wee <swong039@e.ntu.edu.sg>
2019-01-18 15:56:39 +0800

    adding ListView and parsing data from XML

--- app/src/main/java/com/skyblue/learn/top10downloader_kotlin/MainActivity.kt
+++ app/src/main/java/com/skyblue/learn/top10downloader_kotlin/MainActivity.kt
@@ -9,6 +9,23 @@ import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
 
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
@@ -25,9 +42,11 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private class DownloadData : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
-            override fun onPostExecute(result: String?) {
+            override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG, "onPostExecute: parameter is $result")
+//                Log.d(TAG, "onPostExecute: parameter is $result")
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
             }
 
             override fun doInBackground(vararg params: String?): String {
@@ -40,58 +59,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             private fun downloadXML(urlPath: String?): String {
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadXML: The response code was $response")
-
-        //            val inputStream = connection.inputStream
-        //            val inputStreamReader = InputStreamReader(inputStream)
-        //            val reader = BufferedReader(inputStreamReader)
-
-//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//
-//                    val inputBuffer = CharArray(500)
-//                    var charsRead = 0
-//                    while (charsRead <= 0) {
-//                        charsRead = reader.read(inputBuffer)
-//                        if (charsRead > 0) {
-//                            xmlResult.append(String(inputBuffer, 0, charsRead))
-//                        }
-//                    }
-//                    reader.close()
-
-                    val stream = connection.inputStream
-                    stream.buffered().reader().use {
-                        xmlResult.append(it.readText())
-                    }
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-
-//                } catch (e: MalformedURLException) {
-//                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
-//                } catch (e: IOException) {
-//                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-//                } catch (e: SecurityException) {
-//                    Log.e(TAG, "downloadXML: Security exception. Need permissions? ${e.message}")
-//                } catch (e: Exception) {
-//                    Log.e(TAG, "Unknown error: ${e.message}")
-//                }
-                } catch (e: Exception) {
-                    val errorMessage: String = when (e) {
-                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
-                        is IOException -> "downloadXML: IO Exception reading data: ${e.message}"
-                        is SecurityException -> {
-                            "downloadXML: Security exception. Need permissions? ${e.message}"
-                        }
-                        else -> "Unknown error: ${e.message}"
-                    }
-                }
-
-                return "" // If it gets to here, there's been a problem. Returns an empty string
+                return URL(urlPath).readText()
             }
         }
     }
